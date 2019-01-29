96d67812b7214eb10738982229829808390b7a3e
graveja0 <john.graves@vanderbilt.edu>
2019-01-29 17:01:47 -0600

    updates to README

--- R/map-theme.R
+++ R/map-theme.R
@@ -4,5 +4,7 @@ remove_all_axes <- ggplot2::theme(
   axis.ticks = ggplot2::element_blank(),
   panel.border = ggplot2::element_blank(),
   panel.grid = ggplot2::element_blank(),
-  axis.title = ggplot2::element_blank()
+  axis.title = ggplot2::element_blank(),
+  rect = element_blank(), 
+  plot.background = element_blank()
 )
